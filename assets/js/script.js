cfe790016db8c1a04be405080538b72e49b26f7c
ogeraldinh <geraldoduartedemedeirosneto@gmail.com>
2024-05-19 13:54:16 -0300

    Calculadora de Juros Composto v2

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+function calcular_juros() {
+
+  let valor = document.getElementById("valor_inicial").value
+  let taxa_mes = document.getElementById("taxa_mes").value / 100
+  let rendimento_mes = document.getElementById("rendimento_mes").value
+
+  let total = valor * (1 + taxa_mes) ** rendimento_mes
+
+  document.getElementById("total").innerHTML =
+    `Total: R$ ${total.toFixed(2).replace(".", ",")}`
+}
